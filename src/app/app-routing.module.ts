426e33b9d93e061fa532950f0183610a61c4994c
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-03-01 02:09:08 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ListComponent } from './components/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: 'books', component: ListComponent },
+  { path: 'book/:id', component: DetailsComponent },
+  { path: 'add', component: CreateComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/books.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError , Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+
+  constructor(private http:HttpClient ) { }
+
+
+  apiUrl: string = 'https://fakestoreapi.com/products';
+
+headers= new HttpHeaders().set('Content-Type','application/json');
+
+
+
+list():Observable<any> {
+return this.http.get(this.apiUrl);
+}
+
+
+
+
+
+}
